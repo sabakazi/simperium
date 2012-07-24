cae2641d20e71c012dc758975c487b725ca78d8a
Mike Johnston <mike@simperium.com>
2012-07-24 09:31:38 -0700

    major update for testing and feedback

--- ios/Simperium.framework/Versions/A/Headers/Simperium.h
+++ ios/Simperium.framework/Versions/A/Headers/Simperium.h
@@ -8,11 +8,12 @@
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
+#import "SPBucket.h"
 
+@class Simperium;
 @class SPManagedObject;
 @class SPUser;
 @class SPBinaryManager;
-@class SPBucket;
 
 #if TARGET_OS_IPHONE
 @class UIViewController;
@@ -20,28 +21,20 @@
 @class NSWindow;
 #endif
 
-/** Delegate protocol for sync notifications.
+/** Delegate protocol for Simperium system notifications.
  
- You can use SimperiumDelegate if you want explicit callbacks when entities are changed or added. Standard Core Data notifications are also generated, allowing you to update a `UITableView` (for example) in your `NSFetchedResultsControllerDelegate`. 
+ You can use this delegate to respond to authentication events and general errors.
+ 
+ Note you can also use SPBucketDelegate if you want explicit callbacks when objects are changed/added/deleted. Standard Core Data notifications are also generated, allowing you to update a `UITableView` (for example) in your `NSFetchedResultsControllerDelegate`. 
  */
-
 @protocol SimperiumDelegate <NSObject>
 @optional
--(void)indexingWillStart:(NSString *)entityName;
--(void)indexingDidFinish:(NSString *)entityName;
--(void)authenticationSuccessful;
--(void)authenticationFailed;
--(void)authenticationCanceled;
--(void)lightweightMigrationPerformed;
+-(void)simperiumAuthenticationDidSucceed:(Simperium *)simperium;
+-(void)simperiumAuthenticationDidFail:(Simperium *)simperium;
+-(void)simperiumAuthenticationDidCancel:(Simperium *)simperium;
+-(void)simperium:(Simperium *)simperium didFailWithError:(NSError *)error;
 
-// The following will be replaced by NSNotifications in a forthcoming version of the API
--(void)receivedObjectForKey:(NSString *)key version:(NSString *)version data:(NSDictionary *)data;
--(void)objectKeysChanged:(NSSet *)keyArray entityName:(NSString *)entityName;
--(void)objectKeysAdded:(NSSet *)keyArray entityName:(NSString *)entityName;
--(void)objectKeyAcknowledged:(NSString *)key entityName:(NSString *)entityName;
--(void)objectKeyWillBeDeleted:(NSString *)key entityName:(NSString *)entityName;
--(void)objectKeysWillChange:(NSSet *)keyArray entityName:(NSString *)entityName;
-
+// See SPBucketDelegate in SPBucket.h for callbacks that are fired when specific objects change.
 @end
 
 // The main class through which you access Simperium.
@@ -51,19 +44,13 @@
     NSString *appID;
     NSString *APIKey;
     NSString *appURL;
-    NSString *clientID;
-    
+    NSString *clientID;   
+    id<SimperiumDelegate> delegate;  
     SPBinaryManager *binaryManager;
-    NSDictionary *bucketOverrides;
-    
-#if TARGET_OS_IPHONE
-    UIViewController *rootViewController;
-#else
-    NSWindow *window;
-#endif
+    Class loginViewControllerClass;
 }
 
-// Init
+// Initializes Simperium.
 #if TARGET_OS_IPHONE
 -(id)initWithRootViewController:(UIViewController *)controller;
 #else
@@ -71,42 +58,39 @@
 #endif
 
 
-// CORE DATA API
-// Initializes Simperium with the given delegate.
-
-// Starts Simperium with the given application name, access key, and an existing Core Data stack.
+// Starts Simperium with the given credentials (from simperium.com) and an existing Core Data stack.
 -(void)startWithAppID:(NSString *)identifier
                APIKey:(NSString *)key
                 model:(NSManagedObjectModel *)model
-              context:(NSManagedObjectContext *)context
-          coordinator:(NSPersistentStoreCoordinator *)coordinator;
+            context:(NSManagedObjectContext *)context
+        coordinator:(NSPersistentStoreCoordinator *)coordinator;
 
-// Save and sync all changed objects; this is optional (saving your context directly will do the same thing).
+// Save and sync all changed objects. If you're using Core Data, this is just a convenience method
+// (you can also just save your context and Simperium will see the changes).
 -(BOOL)save;
 
-// Returns an object that has the specified simperiumKey.
--(SPManagedObject *)objectForKey:(NSString *)key entityName:(NSString *)entityName;
+// Get a particular bucket (which, for Core Data, corresponds to a particular Entity name in your model).
+-(SPBucket *)bucketForName:(NSString *)name;
 
-// Retrieve past versions of data for a particular object.
--(void)getVersions:(int)numVersions forObject:(SPManagedObject *)object;
-
-// Returns an array of objects for the specified sync keys.
--(NSArray *)objectsForKeys:(NSSet *)keys entityName:(NSString *)entityName;
-
-// Returns an array containing all instances of a particular entity.
--(NSArray *)objectsForEntityName:(NSString *)entityName;
-
-// Efficiently returns the number of objects for a particular entity.
--(NSInteger)numObjectsForEntityName:(NSString *)entityName predicate:(NSPredicate *)predicate;
-
-// Shares an object with a particular user's email address (not yet ready for production use)
--(void)shareObject:(SPManagedObject *)object withEmail:(NSString *)email bucketName:(NSString *)bucketName;
+// Convenience methods for accessing the Core Data stack
+-(NSManagedObjectContext *)managedObjectContext;
+-(NSManagedObjectModel *)managedObjectModel;
+-(NSPersistentStoreCoordinator *)persistentStoreCoordinator;
 
 
 // OTHER
 
 // Set verbose logging on and off for debugging
 -(void)setVerboseLoggingEnabled:(BOOL)on;
+
+// Shares an object with a particular user's email address (not yet ready for production use)
+-(void)shareObject:(SPManagedObject *)object withEmail:(NSString *)email bucketName:(NSString *)bucketName;
+
+// Retrieve past versions of data for a particular object.
+-(void)getVersions:(int)numVersions forObject:(SPManagedObject *)object;
+
+// Alternative to setting delegates on each individual bucket (if you want a single handler for everything)
+-(void)setAllBucketDelegates:(id<SPBucketDelegate>)aDelegate;
 
 // Opens an authentication interface if necessary.
 -(BOOL)authenticateIfNecessary;
@@ -118,30 +102,21 @@
 -(void)enableManualAuthentication;
 -(void)setAuthenticationEnabled:(BOOL)enabled;
 
+/// Set this to true if you need to be able to cancel the authentication dialog.
+@property (nonatomic, assign) BOOL authenticationOptional;
+
 // Clears all locally stored data from the device. Can be used to perform a manual sign out.
 -(void)clearLocalData;
 
-// Adds a delegate so it will be sent notification callbacks.
--(void)addDelegate:(id)delegate;
-
-// Removes a delegate so it no longer receives notification callbacks.
--(void)removeDelegate:(id)delegate;
-
-/// Optional overrides
--(NSString *)bucketOverrideForEntityName:(NSString *)entityName;
--(void)setBucketOverrides:(NSDictionary *)bucketOverrides;
+//-(NSString *)addBinary:(NSData *)binaryData toObject:(SPManagedObject *)object bucketName:(NSString *)bucketName memberName:(NSString *)memberName;
+//-(void)addBinaryWithFilename:(NSString *)filename toObject:(SPManagedObject *)object bucketName:(NSString *)bucketName memberName:(NSString *)memberName;
 
 
+/// A SimperiumDelegate for system callbacks.
+@property (nonatomic,assign) id<SimperiumDelegate> delegate;
 
-// Returns the currently authenticated Simperium user.
+/// Returns the currently authenticated Simperium user.
 @property (nonatomic,retain) SPUser *user;
-
-#if TARGET_OS_IPHONE
-@property (nonatomic, assign) UIViewController *rootViewController;
-#else
-@property (nonatomic, assign) NSWindow *window;
-#endif
-
 
 /// The full URL used to communicate with Simperium.
 @property (nonatomic,readonly) NSString *appURL;
@@ -155,32 +130,25 @@
 /// A hashed, unique ID for this client.
 @property (nonatomic, readonly) NSString *clientID;
 
-/// Set this to true if you need to be able to cancel the authentication dialog
-@property (nonatomic, assign) BOOL authenticationOptional;
+/// You can implement your own subclass of SPLoginViewController to customize authentication.
+@property (nonatomic, assign) Class loginViewControllerClass;
 
 /// Set this if for some reason you want to use multiple Simperium instances (e.g. unit testing).
 @property (copy) NSString *label;
 
-/// Optional overrides
--(void)setBucketOverrides:(NSDictionary *)bucketOverrides;
+/// Optional overrides (used for unit testing).
+@property (nonatomic, copy) NSDictionary *bucketOverrides;
 
-/// All the Simperium delegates
-@property (nonatomic,assign,readonly) NSMutableSet *delegates;
+@property (nonatomic, retain) SPBinaryManager *binaryManager;
+
+
+#if TARGET_OS_IPHONE
+@property (nonatomic, assign) UIViewController *rootViewController;
+#else
+@property (nonatomic, assign) NSWindow *window;
+#endif
 
 /// Saves without syncing (typically not used)
 -(BOOL)saveWithoutSyncing;
-
-/// Binary file management (not yet supported)
-@property (nonatomic, retain) SPBinaryManager *binaryManager;
-
-/// The NSManagedObjectContext used by Simperium.
-@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
-
-/// The NSManagedObjectModel used by Simperium.
-@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
-
-/// The NSPersistentStoreCoordinator used by Simperium.
-@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
 
 @end
